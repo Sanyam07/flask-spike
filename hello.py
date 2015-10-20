6a30109d6f4f793767685a790c68224f5d88e23e
Scott Skender <sjskender@gmail.com>
2015-10-20 12:13:36 -0600

    Load & print some sample data

--- hello.py
+++ hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    import graphlab
+    songs = graphlab.SFrame.read_csv('http://s3.amazonaws.com/dato-datasets/millionsong/song_data.csv')
+    print songs
+    return "done"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
